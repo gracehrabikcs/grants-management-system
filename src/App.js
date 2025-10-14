19771fa59b24b4b3b2e2a8de4488a0037e16821b
mgallegos44 <mgallegos@css.edu>
2025-10-14 12:04:30 -0500

    Test

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Sidebar from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
+//test comment
 
 function App() {
   return (
